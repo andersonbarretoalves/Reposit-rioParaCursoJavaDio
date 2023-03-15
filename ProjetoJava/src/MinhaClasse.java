3de7bf177c3e22cf4d58859427d76bad17eff7b1
andersonbarretoalves <andersonguitar2@gmail.com>
2023-03-15 18:44:22 -0300

    Metodos e Variáveis

--- ProjetoJava/src/MinhaClasse.java
+++ ProjetoJava/src/MinhaClasse.java
@@ -1,5 +1,13 @@
 public class MinhaClasse {
-    public static void main (String[] args){
-        System.out.println("Olá, Sejam Bem-vindos!");
+    public static void main (String [] args) {
+        String primeiroNome = "Anderson";
+        String segundoNome = "Alves";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
