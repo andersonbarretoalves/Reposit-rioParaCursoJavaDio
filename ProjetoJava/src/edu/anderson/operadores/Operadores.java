041ac56823566e615492c31f20a3ba1116ab9a88
andersonbarretoalves <andersonguitar2@gmail.com>
2023-03-17 11:10:39 -0300

    Aula de Operadores

---
+++ ProjetoJava/src/edu/anderson/operadores/Operadores.java
@@ -0,0 +1,16 @@
+package edu.anderson.operadores;
+
+public class Operadores {
+    public static void main (String[] args) {
+        double soma = 10.5 + 15.7; // adição quando usado em texto (String) é feito a concatenação Ex: "nome" + "sobrenome".
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20 / 4);
+        String primeiroNome = "Anderson";
+        String segundoNome = "Alves";
+        System.out.println(primeiroNome + " " + segundoNome);
+    }
+    
+}
