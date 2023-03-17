c5e2321a56f2f1c2fbf01a21b45904cb5c4b553a
andersonbarretoalves <andersonguitar2@gmail.com>
2023-03-17 08:40:07 -0300

    Tipos de Variáveis

--- ProjetoJava/src/edu/anderson/tiposEVariaveis/TiposVariaveis.java
+++ ProjetoJava/src/edu/anderson/tiposEVariaveis/TiposVariaveis.java
@@ -1,5 +1,15 @@
 package edu.anderson.tiposEVariaveis;
 
 public class TiposVariaveis {
+    public static void main (String[] args) {
+
+        short numeroCurto = 1;
+        double numeroNormal = 1320;
+        short numeroCurto2 = (short) numeroNormal; //Casting é usado para a conversão de tipos de variáves, no caso do "numeroNormal em short".
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+        final double VALOR_DE_PI = 3.14; //finel é usado para numero (Constante), onde não pode ser alterado. Declarado em Caixa Alta.
+    }
     
 }
