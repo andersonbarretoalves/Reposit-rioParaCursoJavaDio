8354d775935feb2cb500b0573f54e729f38467b4
andersonbarretoalves <andersonguitar2@gmail.com>
2023-03-21 15:18:25 -0300

    Criando um Controle para uma SmartTV.

--- ProjetoJava/src/edu/anderson/metodos/Usuario.java
+++ ProjetoJava/src/edu/anderson/metodos/Usuario.java
@@ -3,9 +3,30 @@ package edu.anderson.metodos;
 public class Usuario {
     public static void main (String[] args) throws Exception {
         SmartTv smarTv = new SmartTv();
-        System.out.println("TV ligada " + smarTv.ligada);
-        System.out.println("Canal " + smarTv.canal);
-        System.out.println("Volume " + smarTv.volume);
+
+        smarTv.diminuirVolume();
+        smarTv.diminuirVolume();
+        smarTv.diminuirVolume();
+        smarTv.aumentarVolume();
+
+        System.out.println("Volume atual: " + smarTv.volume);
+
+        System.out.println("Canal: " + smarTv.canal);
+
+        smarTv.mudarCanal(13);
+        smarTv.diminuirCanal();
+        smarTv.aumentarCanal(); 
+                         
+        //System.out.println("TV ligada: " + smarTv.ligada);       
+                
+        smarTv.ligar();
+        System.out.println("Agora a TV está: " + smarTv.ligada + " Ligada!");
+
+        smarTv.desligar();
+        System.out.println("Agora a TV está: " + smarTv.ligada + " Desligada!");
+
+        
+
     }
     
 }
