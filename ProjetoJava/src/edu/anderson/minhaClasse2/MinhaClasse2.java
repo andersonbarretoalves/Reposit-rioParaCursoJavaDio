6f1407ae539e238894947a2291c7cde0dae7f820
andersonbarretoalves <andersonguitar2@gmail.com>
2023-03-24 11:06:56 -0300

    Modificação do Nome do Arquivo

--- ProjetoJava/src/edu/anderson/minhaClasse2/MinhaClasse2.java
+++ ProjetoJava/src/edu/anderson/minhaClasse2/MinhaClasse2.java
@@ -1,6 +1,6 @@
-package edu.anderson.minhaClasse;
+package edu.anderson.minhaClasse2;
 
-public class MinhaClasse {
+public class MinhaClasse2 {
     public static void main(String[] args) {
         System.out.println("Oi, Fui executado pelo terminal.");
     }
