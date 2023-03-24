a245b64e324f1b5826137e8964bd28ab00cd811e
andersonbarretoalves <andersonguitar2@gmail.com>
2023-03-24 11:20:07 -0300

    Compilado o arquivo MinhaClasse2

--- ProjetoJava/src/edu/anderson/minhaClasse2/MinhaClasse2.java
+++ ProjetoJava/src/edu/anderson/minhaClasse2/MinhaClasse2.java
@@ -2,7 +2,6 @@ package edu.anderson.minhaClasse2;
 
 public class MinhaClasse2 {
     public static void main(String[] args) {
-        System.out.println("Oi, Fui executado pelo terminal.");
+        System.out.println("Oi Turma, Fui executado pelo terminal.");
     }
-    
 }
