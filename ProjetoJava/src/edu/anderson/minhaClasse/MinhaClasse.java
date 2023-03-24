1aff432bd731fe47502493e683b520bc601af294
andersonbarretoalves <andersonguitar2@gmail.com>
2023-03-24 08:22:52 -0300

    Atualização da versão do Java

---
+++ ProjetoJava/src/edu/anderson/minhaClasse/MinhaClasse.java
@@ -0,0 +1,8 @@
+package edu.anderson.minhaClasse;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Oi, Fui executado pelo terminal.");
+    }
+    
+}
