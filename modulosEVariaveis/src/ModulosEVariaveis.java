e1228e67f54c29eee9b49eef31ad473aad347ba1
andersonbarretoalves <andersonguitar2@gmail.com>
2023-03-26 20:06:21 -0300

    Alterado o local do projeto ModulosEVariaveis

--- modulosEVariaveis/src/ModulosEVariaveis.java
+++ modulosEVariaveis/src/ModulosEVariaveis.java
@@ -1,13 +1,10 @@
-package edu.anderson.modulosEVariaveis;
-public class MinhaClasse {
+public class ModulosEVariaveis {
     public static void main (String [] args) {
         String primeiroNome = "Anderson";
         String segundoNome = "Alves";
-
         String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
         System.out.println(nomeCompleto);
     }
-
     public static String nomeCompleto (String primeiroNome, String segundoNome){
         return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
