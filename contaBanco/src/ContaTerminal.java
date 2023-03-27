0a4a2c8311749983e2171e5225c3f12f770c549e
andersonbarretoalves <andersonguitar2@gmail.com>
2023-03-27 08:54:25 -0300

    Enviando a Atualização do Projeto

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         System.out.println("Bem vindo ao Banco Dio!");
 
-        try (Scanner sc = new Scanner(System.in)) {          
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);          
 
             System.out.println("Por favor digite o numero da Agência: ");
             String agencia = sc.next();
@@ -13,17 +14,22 @@ public class ContaTerminal {
             System.out.println("Por favor digite o numero da Conta: ");
             int conta = sc.nextInt();
 
-            System.out.println("Por favor digite o seu nome Completo: ");
-            String nomeCliente = sc.nextLine();
+            System.out.println("Por favor digite o seu Primeiro Nome: ");
+            String nome = sc.next();
+
+            System.out.println("Por favor digite o seu sobrenome: ");
+            String sobreNome = sc.next();
 
             System.out.println("Favor digitar o valor depositado: ");
             double saldo = sc.nextDouble();
 
-            System.out.println("Bem vindo " + nomeCliente + "Sua conta foi criada com sucesso no Banco Dio!");
+            System.out.println("Bem vindo " + nome + " " + sobreNome + ", Sua conta foi criada com sucesso no Banco Dio!");
             System.out.println("Agência: " + agencia);
             System.out.println("Conta: " + conta);
             System.out.println("Seu saldo é de: " + saldo);
-        }  
+
+            sc.close();    
+        
                
     }
 }
